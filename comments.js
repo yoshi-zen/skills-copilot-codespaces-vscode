917806b612111deb49290800761f5b64dcce6433
齋藤　善仁 <124111789+yoshi-zen@users.noreply.github.com>
2024-06-20 06:43:21 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -4,22 +4,3 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-// create a comments array
-const comments = [];
-
-// get all comments
-app.get('/comments', (req, res) => {
-  res.json(comments);
-});
-
-// post a comment
-app.post('/comments', (req, res) => {
-  const comment = req.body;
-  comments.push(comment);
-  res.json(comment);
-});
-
-// start server
-app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001');
-});
