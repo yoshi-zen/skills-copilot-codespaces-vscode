2b6c7a30d838d32f35ceb03ab7bd04c47df9ffcb
齋藤　善仁 <124111789+yoshi-zen@users.noreply.github.com>
2024-06-20 06:40:20 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,35 +1,25 @@
 // create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-// GET method route
-app.get('/comments', function(req, res) {
-    // read from file
-    fs.readFile('comments.json', 'utf8', function(err, data) {
-        if (err) {
-            console.log(err);
-            res.status(404).send('File not found');
-        }
-        else {
-            res.send(data);
-        }
-    });
+// create a comments array
+const comments = [];
+
+// get all comments
+app.get('/comments', (req, res) => {
+  res.json(comments);
 });
 
-// POST method route
-app.post('/comments', function(req, res) {
-    // read from file
-    fs.readFile('comments.json', 'utf8', function(err, data) {
-        if (err) {
-            console.log(err);
-            res.status(404).send('File not found');
-        }
-        else {
-            var comments = JSON.parse(data);
-            var newComment = {
-                id: comments.length + 1,
+// post a comment
+app.post('/comments', (req, res) => {
+  const comment = req.body;
+  comments.push(comment);
+  res.json(comment);
+});
+
+// start server
+app.listen(3001, () => {
+  console.log('Server is running on http://localhost:3001');
+});
